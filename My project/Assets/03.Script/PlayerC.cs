bb7f65a6630779baf20e74583c1633dbcd328c29
BalPudding <buslaut@naver.com>
2022-10-21 16:25:12 +0900

    20221021

--- My project/Assets/03.Script/PlayerC.cs
+++ My project/Assets/03.Script/PlayerC.cs
@@ -13,6 +13,13 @@ public class PlayerC : MonoBehaviour
     private bool oneHit = false;
     private bool twoHit = false;
     private bool healCycle = false;
+    private bool oneFall = false;
+    private bool twoFall = false;
+    public GameObject hitplatform;
+    private bool blink = false;
+    float fade;
+    public GameObject shieldObj;
+    private bool shield = true;
 
 
 
@@ -27,23 +34,60 @@ public class PlayerC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //hp최댓값제한
+        if(hp>3)
+        {
+            hp = 3;
+        }
+        //보호막
+        if(shield == true)
+        {
+            shieldObj.SetActive(true);
+        }
         //1히트
-        if (transform.position.x < -2.9f)
+        if (shield == true && oneHit == true)
+        {
+            oneHit = false;
+            shield = false;
+            shieldObj.SetActive(false);
+        }
+        else if (transform.position.x < -2.9f)
         { oneHit = false; }
         else if(oneHit == true)
         {
             transform.position = Vector3.Lerp(transform.position, new Vector2(-3,-3), 0.01f);
         }
         //2히트
-        if (transform.position.x < -5.9f)
+        if (shield == true && twoHit == true)
+        {
+            twoHit = false;
+            shield = false;
+            shieldObj.SetActive(false);
+        }
+        if(transform.position.x < -5.9f)
         { twoHit = false; }
         else if (twoHit == true)
         {
             transform.position = Vector3.Lerp(transform.position, new Vector2(-6, -3), 0.01f);
         }
+        //낙사 1히트
+        if (transform.position.x < -2.9f)
+        { oneFall = false; }
+        else if (oneFall == true)
+        {
+            transform.position = new Vector2(-3, -3);
+        }
+
+        //낙사 2히트
+        if (transform.position.x < -5.9f)
+        { twoFall = false; }
+        else if (twoFall == true)
+        {
+            transform.position = new Vector2(-6, -3);
+        }
 
         //힐사이클
-        if(healCycle == true)
+        if (healCycle == true)
         {
             oneHit = false;
             twoHit = false;
@@ -70,6 +114,25 @@ public class PlayerC : MonoBehaviour
 
             jumpcount += 1;
         }
+        //블링크
+        if (blink == true)
+        {
+
+            if (fade < 0.5f)
+            { 
+                spriteRenderer.color = new Color(1f, 1f, 1f, 1f - fade); 
+            }
+            else
+            {
+                spriteRenderer.color = new Color(1f, 1f, 1f, fade);
+                if(fade>1f)
+                {
+                    fade = 0;
+                }
+            }
+            fade += Time.deltaTime;
+        }
+
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -98,7 +161,19 @@ public class PlayerC : MonoBehaviour
             other.gameObject.SetActive(false);
         }
         //데스 태그
-        if (other.gameObject.tag == "Enemy" && hp == 3)
+        if (other.gameObject.tag == "Death" && hp == 3)
+        {
+            oneFall = true;
+            hp -= 1;
+            OnDamaged(gameObject.transform.position);
+        }
+        else if (other.gameObject.tag == "Death" && hp == 2)
+        {
+            twoFall = true;
+            hp -= 1;
+            OnDamaged(gameObject.transform.position);
+        }
+        else if (other.gameObject.tag == "Death" && hp == 1)
         {
             Die();
         }
@@ -116,13 +191,17 @@ public class PlayerC : MonoBehaviour
     }
     void OnDamaged(Vector2 targetPos2)
     {
+        blink = true;
+        hitplatform.SetActive(true);
         gameObject.layer = 11;
-        spriteRenderer.color = new Color(1f, 1f, 1f, 0.5f);
+        //spriteRenderer.color = new Color(1f, 1f, 1f, 0.5f);
         Invoke("OffDamaged", 3);
     }
     void OffDamaged()
     {
+        blink = false;
+        hitplatform.SetActive(false);
         gameObject.layer = 0;
-        spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
+        //spriteRenderer.color = new Color(1f, 1f, 1f, 1f);
     }
 }
